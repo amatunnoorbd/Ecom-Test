e90a9634c548e3a394ca05ed60e2f67190d8c4db
sumaiya4646 <sumaiyamushtarimim@gmail.com>
2024-10-06 10:19:07 +0600

    change postcss file

--- src/components/navbar/Navlink.jsx
+++ src/components/navbar/Navlink.jsx
@@ -1,21 +1,21 @@
 import { IoIosArrowDown } from "react-icons/io";
 import { IoHome } from "react-icons/io5";
+import Link from 'next/link';
 
-
-const Navas = () => {
+const Navlink = () => {
     return (
         <div className="flex items-center pl-28 border-t border-b border-[#e8d3d3]">
-            <a to='/' className="p-3 text-2xl border-r border-[#e8d3d3] hover:text-red-600">
+            <Link href='/' className="p-3 text-2xl border-r border-[#e8d3d3] hover:text-red-600">
                 <IoHome />
-            </a>
+            </Link>
 
             {/* 1 */}
             <div className="dropdown">
                 <button className="flex gap-2 items-center dropbtn p-3 border-r border-[#e8d3d3]"><span>ATTAR</span> <IoIosArrowDown className="mt-1" /></button>
                 <div className="dropdown-content space-y-2 py-5 px-4 shadow-2xl">
-                    <a to="/wholesaleAttar" className="border-b border-[#d4caca] pb-2"  >WHOLESALE ATTAR</a>
-                    <a to="/products" className="border-b border-[#d4caca] pb-2"  >COMBO & GIFTS</a>
-                    <a to="/products"  >ATTAR JERSEY COMBO</a>
+                    <Link href="/wholesaleAttar" className="border-b border-[#d4caca] pb-2">WHOLESALE ATTAR</Link>
+                    <Link href="/products" className="border-b border-[#d4caca] pb-2">COMBO & GIFTS</Link>
+                    <Link href="/products">ATTAR JERSEY COMBO</Link>
                 </div>
             </div>
 
@@ -23,23 +23,23 @@ const Navas = () => {
             <div className="dropdown">
                 <button className="flex gap-2 items-center dropbtn p-3 border-r border-[#e8d3d3]"><span>CLOTHING</span> <IoIosArrowDown className="mt-1" /></button>
                 <div className="dropdown-content space-y-2 py-5 px-4 shadow-2xl">
-                    <a to="/panjabi" className="border-b border-[#d4caca] pb-2"  >PANJABI</a>
-                    <a to="/dawahJersey" >DAWAH JERSEY</a>
+                    <Link href="/panjabi" className="border-b border-[#d4caca] pb-2">PANJABI</Link>
+                    <Link href="/dawahJersey">DAWAH JERSEY</Link>
                 </div>
             </div>
 
             {/* 3 */}
             <div className="dropdown">
-                <a to="/products">
+                <Link href="/products">
                     <button className="flex gap-2 items-center dropbtn p-3 border-r border-[#e8d3d3]"><span>DAWAH CANVAS</span></button>
-                </a>
+                </Link>
             </div>
 
             {/* 4 */}
             <div className="dropdown">
                 <button className="flex gap-2 items-center dropbtn p-3 border-r border-[#e8d3d3]"><span>OTHERS</span> <IoIosArrowDown className="mt-1" /></button>
                 <div className="dropdown-content space-y-2 py-5 px-4 shadow-2xl">
-                    <a to="/rechargeFan">RECHARGEABLE FAN</a>
+                    <Link href="/rechargeFan">RECHARGEABLE FAN</Link>
                 </div>
             </div>
 
@@ -47,8 +47,8 @@ const Navas = () => {
             <div className="dropdown">
                 <button className="flex gap-2 items-center dropbtn p-3 border-r border-[#e8d3d3]"><span>COMBO OFFERS</span> <IoIosArrowDown className="mt-1" /></button>
                 <div className="dropdown-content space-y-2 py-5 px-4 shadow-2xl">
-                    <a to="/products" className="border-b border-[#d4caca] pb-2"  >DAWAH CAP COMBO</a>
-                    <a to='/attarAndShirt' >10 PCS POPULAR ATTAR + T-SHIRT COMBO</a>
+                    <Link href="/products" className="border-b border-[#d4caca] pb-2">DAWAH CAP COMBO</Link>
+                    <Link href='/attarAndShirt'>10 PCS POPULAR ATTAR + T-SHIRT COMBO</Link>
                 </div>
             </div>
 
@@ -56,24 +56,22 @@ const Navas = () => {
             <div className="dropdown">
                 <button className="flex gap-2 items-center dropbtn p-3 border-r border-[#e8d3d3]"><span>FOOD</span> <IoIosArrowDown className="mt-1" /></button>
                 <div className="dropdown-content space-y-2 py-5 px-4 shadow-2xl">
-                    <a to="/products" className="border-b border-[#d4caca] pb-2"  >MEDJOOL DATES</a>
-                    <a to="/litchiHoney">LITCHI FLOWER HONEY</a>
+                    <Link href="/products" className="border-b border-[#d4caca] pb-2">MEDJOOL DATES</Link>
+                    <Link href="/litchiHoney">LITCHI FLOWER HONEY</Link>
                 </div>
             </div>
 
             {/* 7 */}
             <div className="dropdown">
-                <button className="flex gap-2 items-center dropbtn p-3 border-r border-[#e8d3d3]"><span>WOMEN'S CLOTHING</span> <IoIosArrowDown className="mt-1" /></button>
+                <button className="flex gap-2 items-center dropbtn p-3 border-r border-[#e8d3d3]"><span>WOMEN&apos;S CLOTHING</span> <IoIosArrowDown className="mt-1" /></button>
                 <div className="dropdown-content space-y-2 py-5 px-4 shadow-2xl">
-                    <a to="/products" className="border-b border-[#d4caca] pb-2"  >ESSENTIAL LONG KHIMAR</a>
-                    <a to="/products" className="border-b border-[#d4caca] pb-2">SALAT KHIMAR</a>
-                    <a to="/products">BORKA</a>
+                    <Link href="/products" className="border-b border-[#d4caca] pb-2">ESSENTIAL LONG KHIMAR</Link>
+                    <Link href="/products" className="border-b border-[#d4caca] pb-2">SALAT KHIMAR</Link>
+                    <Link href="/products">BORKA</Link>
                 </div>
             </div>
-
-
         </div>
     );
 };
 
-export default Navas;
+export default Navlink;
